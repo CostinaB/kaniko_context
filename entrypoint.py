6c6513cac53be4c6f53e5416dde575484aa214ab
Batica Costina <costina.batica@bigstep.com>
2019-09-04 04:19:08 -0700

    fix

--- entrypoint.py
+++ entrypoint.py
@@ -11,6 +11,7 @@ from bdl_client.rest import ApiException
 from nbconvert.preprocessors import ExecutePreprocessor
 from bdl_client.configuration import Configuration
 from bdl_client.api_client import ApiClient
+import nbformat
 
 NBFORMAT_VERSION = 4
 
@@ -29,13 +30,13 @@ class ExecuteNotebook():
       result = self.api.get_notebook_by_datalake_id_and_version(self.api_key, datalake_id, notebook_name, notebook_version)
       result = result.replace("'", '"')
       result = json.loads(result)
-      notebook = notebook_model_from_db(result)
+      notebook = self.notebook_model_from_db(result)
       content = FileContentsManager()
-      content.save(model, '/notebook.ipynb')
+      content.save(notebook, '/notebook.ipynb')
     except ApiException as e:
       print(e.body)['message']
 
-  def execute_notebook():
+  def execute_notebook(self):
     with open('/notebook.ipynb') as f:
       nb = nbformat.read(f, as_version=4)
     ep = ExecutePreprocessor(timeout=600, allow_errors=True)
@@ -43,13 +44,13 @@ class ExecuteNotebook():
     with open('/executed_notebook.ipynb', mode='wt') as f:
       nbformat.write(nb, f)
 
-  def reads_base64(nb, as_version=NBFORMAT_VERSION):
+  def reads_base64(self, nb, as_version=NBFORMAT_VERSION):
     """
     Read a notebook from base64.
     """
     return reads(b64decode(nb).decode('utf-8'), as_version=as_version)
 
-  def base_model(path):
+  def base_model(self, path):
     return {
       "name": path.rsplit('/', 1)[-1],
       "path": path,
@@ -61,16 +62,16 @@ class ExecuteNotebook():
       "mimetype": None,
     }
 
-  def notebook_model_from_db(record):
+  def notebook_model_from_db(self, record):
     """
     Build a notebook model from database record.
     """
-    path = record['name']
-    model = base_model(path)
+    path = record.name
+    model = self.base_model(path)
     model['type'] = 'notebook'
-    model['last_modified'] = record['updatedTimestampSeconds']
-    model['created'] = record['createdTimestampSeconds']
-    content = reads_base64(record['contentBase64Encoded'])
+    model['last_modified'] = record.updated_timestamp_seconds
+    model['created'] = record.created_timestamp_seconds
+    content = self.reads_base64(record.content_base64_encoded)
     model['content'] = content
     model['format'] = 'json'
     return model
